fdf1071b87699b48c352d6e1853f59202c2de392
Matsvei Niaverau <matvej2002@gmail.com>
2020-10-23 09:35:08 +0200

    nabu: Inherit from common tree
    Change-Id: I1e13b71d9218cdb1bd8e3ea023dfb79b1d37cb9d

---
+++ BoardConfig.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8150-common
+include device/xiaomi/sm8150-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/nabu
+
+BUILD_BROKEN_DUP_RULES := true
+
+# Display
+TARGET_SCREEN_DENSITY := 360
+
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/xiaomi/nabu.config
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Inherit from the proprietary version
+include vendor/xiaomi/nabu/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_IS_VAB := true
+TARGET_IS_TABLET := true
+
+# Inherit from sm8150-common
+$(call inherit-product, device/xiaomi/sm8150-common/msmnile.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1600
+
+# Camera
+PRODUCT_PACKAGES += \
+    libMegviiFacepp-0.5.2 \
+    libmegface
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/nabu/nabu-vendor.mk)
